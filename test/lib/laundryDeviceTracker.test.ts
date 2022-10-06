47a60c213d679b2b705a95feb97a4f5571920dcc
fyodor <fyodor.yakimchouk@icloud.com>
2022-10-07 10:43:49 +1300

    add more unit tests

--- test/lib/laundryDeviceTracker.test.ts
+++ test/lib/laundryDeviceTracker.test.ts
@@ -4,14 +4,16 @@ import {LaundryDeviceTracker} from '../../src/lib/laundryDeviceTracker';
 import {log} from '../__utils__/log';
 import {PushGateway} from '../../src/lib/pushGateway';
 import {emptyConfig} from '../__utils__/config';
-import {PlatformConfig} from 'homebridge';
+import {PlatformConfig, Service} from 'homebridge';
 import {NotifyConfig} from '../../src/interfaces/notifyConfig';
 import {MockedObject} from 'ts-jest/dist/utils/testing';
 import {HomebridgeAPI} from 'homebridge/lib/api';
+import {PlatformAccessory} from 'homebridge/lib/platformAccessory';
 
 let mockedLaundryDeviceInstance: Partial<LaundryDevice>;
 
 jest.mock('../../src/lib/pushGateway');
+jest.mock('homebridge/lib/platformAccessory');
 jest.mock('../../src/lib/laundryDevice', () => {
   return {
     LaundryDevice: jest.fn().mockImplementation(() => {
@@ -26,16 +28,26 @@ jest.mock('../../src/lib/laundryDevice', () => {
 });
 
 const mockedLaundryDevice = mocked(LaundryDevice, true);
+const mockedPlatformAccessory = mocked(PlatformAccessory, true);
 
 describe('LaundryDeviceTracker', () => {
   let landryDeviceTracker: LaundryDeviceTracker;
   let config: PlatformConfig & NotifyConfig;
   let mockedPushGatewayInstance: MockedObject<PushGateway>;
   let api: HomebridgeAPI;
+  let mockedService: Service;
+  let dummyAccessory: PlatformAccessory;
 
   beforeEach(() => {
     config = { ...emptyConfig };
     api = new HomebridgeAPI();
+
+    mockedPlatformAccessory.mockReset();
+    dummyAccessory = new PlatformAccessory('does not', 'matter');
+
+    mockedService = {
+      setCharacteristic: jest.fn(),
+    } as Partial<Service> as unknown as Service;
 
     config.laundryDevices = [
       {
@@ -125,5 +137,36 @@ describe('LaundryDeviceTracker', () => {
     jest.setSystemTime(new Date('1970-01-01T00:00:22'));
     mockedLaundryDeviceInstance.emit!('refresh');
     expect(mockedPushGatewayInstance.send).toHaveBeenCalledWith('end message');
+  });
+
+  it('should flip accessory to on when device reached power value and work for the right duration', async () => {
+    config.laundryDevices[0].exposeStateSwitch = true;
+    landryDeviceTracker.accessory = dummyAccessory;
+    jest.setSystemTime(new Date('1970-01-01T00:00:00'));
+    landryDeviceTracker.init();
+    mockedPlatformAccessory.mock.instances[0].getService = jest.fn(() => mockedService);
+    mockedLaundryDeviceInstance.emit!('refresh');
+    mockedLaundryDeviceInstance.emit!('data', { dps: {'42': 101 }});
+    jest.setSystemTime(new Date('1970-01-01T00:00:11'));
+    mockedLaundryDeviceInstance.emit!('refresh');
+    expect(mockedPlatformAccessory.mock.instances[0].getService).toHaveBeenCalledWith(api.hap.Service.Switch);
+    expect(mockedService.setCharacteristic).toHaveBeenCalledWith(api.hap.Characteristic.On, true);
+  });
+
+  it('should flip accessory to off when device reached active state and then finished', async () => {
+    config.laundryDevices[0].exposeStateSwitch = true;
+    landryDeviceTracker.accessory = dummyAccessory;
+    jest.setSystemTime(new Date('1970-01-01T00:00:00'));
+    landryDeviceTracker.init();
+    mockedPlatformAccessory.mock.instances[0].getService = jest.fn(() => mockedService);
+    mockedLaundryDeviceInstance.emit!('refresh');
+    mockedLaundryDeviceInstance.emit!('data', { dps: {'42': 101 }});
+    jest.setSystemTime(new Date('1970-01-01T00:00:11'));
+    mockedLaundryDeviceInstance.emit!('refresh');
+    mockedLaundryDeviceInstance.emit!('data', { dps: {'42': 40 }});
+    jest.setSystemTime(new Date('1970-01-01T00:00:22'));
+    mockedLaundryDeviceInstance.emit!('refresh');
+    expect(mockedPlatformAccessory.mock.instances[0].getService).toHaveBeenCalledWith(api.hap.Service.Switch);
+    expect(mockedService.setCharacteristic).toHaveBeenCalledWith(api.hap.Characteristic.On, false);
   });
 });
